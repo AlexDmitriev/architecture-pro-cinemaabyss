012720e9092b27263383234519bd555fdc50e4a7
Alex Dmitriev <alex@alexdmitriev.com>
2026-01-21 20:52:31 +0300

    Fix split_clients for nginx

--- src/microservices/proxy/entrypoint.sh
+++ src/microservices/proxy/entrypoint.sh
@@ -5,6 +5,11 @@ export MONOLITH_HOST=$(echo ${MONOLITH_URL} | sed 's|http://||' | sed 's|https:/
 export MOVIES_SERVICE_HOST=$(echo ${MOVIES_SERVICE_URL} | sed 's|http://||' | sed 's|https://||')
 export EVENTS_SERVICE_HOST=$(echo ${EVENTS_SERVICE_URL} | sed 's|http://||' | sed 's|https://||')
 
+# Если gradual migration выключен, устанавливаем процент в 100 (все запросы на movies-service)
+if [ "${GRADUAL_MIGRATION}" != "true" ]; then
+    export MOVIES_MIGRATION_PERCENT="100"
+fi
+
 # Подставляем переменные окружения в конфиг nginx
 envsubst '${PORT} ${MONOLITH_URL} ${MONOLITH_HOST} ${MOVIES_SERVICE_URL} ${MOVIES_SERVICE_HOST} ${EVENTS_SERVICE_URL} ${EVENTS_SERVICE_HOST} ${GRADUAL_MIGRATION} ${MOVIES_MIGRATION_PERCENT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
